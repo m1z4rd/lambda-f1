b888591dd03dae02336e3e7a043fb03869665391
Christian Flores <mizard@gmail.com>
2023-05-16 17:25:12 -0600

    Add some lines to solve CORS stuff

--- src/api/getDataSprints.js
+++ src/api/getDataSprints.js
@@ -16,6 +16,11 @@ let getDataSprints = async () => {
 
         response = {
             statusCode: 200,
+            headers:{
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
             body: JSON.stringify(item)
         };
     }
